062ab80a18efba58151978924a95d84aaf78992c
tangtanglove <dai_hang_love@126.com>
2019-05-20 19:45:24 +0800

    修正代码

--- resources/app/src/pages/Auth/Login.js
+++ resources/app/src/pages/Auth/Login.js
@@ -142,9 +142,6 @@ class LoginPage extends Component {
             <Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>
               <FormattedMessage id="app.login.remember-me" />
             </Checkbox>
-            <a style={{ float: 'right' }} href="">
-              <FormattedMessage id="app.login.forgot-password" />
-            </a>
           </div>
           <Submit loading={submitting}>
             <FormattedMessage id="app.login.login" />
